313df4e9943d1395ad66453a4ba6b03655e54f4b
lcy958756276-dev <lcy958756276@gmail.com>
2025-12-23 14:43:03 +0800

    initial commit

---
+++ data/utils.py
@@ -0,0 +1,132 @@
+from typing import Optional
+
+from datasets import IterableDataset,load_dataset
+
+# def load_medical_dataset(path):
+#     with open(path,"r",encoding="utf-8") as f:
+#         lines = f.read().splitlines()
+#     for id,line in enumerate(lines):
+#         print(f"{line}")
+
+
+#load_medical_dataset("D:\\vscode--llm\\stage3_data\\llm_data\\medical.train")
+
+
+from typing import Optional
+
+from datasets import IterableDataset,load_dataset
+# from torch.utils.data import Dataset
+from torch.utils.data import Dataset as TorchDataset
+from datasets import Dataset as HFDataset
+import json
+
+
+categories = set()
+num=0
+class PeopleDaily(TorchDataset):
+    def __init__(self, data_file):
+        self.data = self.load_data(data_file)
+        # self.dataset = Dataset.from_dict(self.data)
+        self.dataset =  HFDataset.from_list(self.data)
+    
+    def load_data(self, data_file):
+        #Data = {}
+        global num
+        Data=[]
+        with open(data_file, 'rt', encoding='utf-8') as f:
+            for idx, line in enumerate(f.read().split('\n\n')):
+                if not line:
+                    break
+                sentence, labels = '', []
+                for i, item in enumerate(line.split('\n')):
+                    char, tag = item.split(' ')
+                    sentence += char
+                    if tag.startswith('B'):
+                        # labels.append([i, i, char, tag[2:]]) # Remove the B- or I-
+                        labels.append([char, tag[2:]]) # Remove the B- or I-
+                        categories.add(tag[2:])
+                    elif tag.startswith('I'):
+                        #labels[-1][1] = i
+                        #labels[-1][2] += char
+                        labels[-1][0]+=char
+                # Data[idx] = {
+                #     'sentence': sentence, 
+                #     'labels': labels
+                # }
+#                 {
+#   0: {"sentence": s1, "labels": l1},
+#   1: {"sentence": s2, "labels": l2},
+#   2: {"sentence": s3, "labels": l3}
+# }
+                Data.append({
+                    'sentence': sentence, 
+                     'labels': labels
+                })
+                num+=1
+
+        return Data
+
+    def __len__(self):
+        return len(self.data)
+
+    # def __getitem__(self, idx):
+    #     return self.data[idx]
+    def to_hf(self):
+        return self.dataset
+
+    def __getitem__(self, idx):
+    # 如果是 slice，比如 [:10]
+        if isinstance(idx, slice):
+        # 返回一个列表（或新的 Dataset）
+            return [self.data[i] for i in range(*idx.indices(len(self.data)))]
+
+    # 普通整数 index
+        return self.data[idx]
+
+
+train_data = PeopleDaily('D:\\vscode--llm\\stage3_data\\llm_data\\medical.train').to_hf()
+print(f"{num}")
+# valid_data = PeopleDaily('D:\\vscode--llm\\stage3_data\\llm_data\\medical.dev').to_hf()
+# test_data = PeopleDaily('D:\\vscode--llm\\stage3_data\\llm_data\\medical.test').to_hf()
+
+# print(type(train_data[0]))---->dict
+# print(train_data[1])
+# print(train_data[2])
+# print(train_data[4])
+# print(train_data[5])
+
+# {'sentence': '现头昏口苦', 'labels': [['口苦', '临床表现']]}
+# {'sentence': '目的观察复方丁香开胃贴外敷神阙穴治疗慢性心功能不全伴功能性消化不良的临床疗效', 'labels': [['复方丁香开胃贴', '中医治疗'], ['心功能不全伴功能性消化不良', '西医诊断']]}
+# {'sentence': '舒肝和胃消痞汤；功能性消化不良', 'labels': [['功能性消化不良', '西医诊断']]}
+# {'sentence': '治疗组采用复方蜥蜴散不同微粒组合剂（密点麻蜥、炙黄芪、焦乌梅、炒白芍、三七、半枝莲等）治疗', 'labels': [['复方蜥蜴散', '方剂'], ['密点麻蜥', '中药'], ['炙黄芪', '中药'], ['焦乌梅', '中药'], ['炒
+# 白芍', '中药'], ['三七', '中药'], ['半枝莲', '中药']]}
+# {'sentence': '经检查诊断为“缩窄性心包炎”', 'labels': [['缩窄性心包炎', '西医诊断']]}
+
+
+SYSTEM_prompt=(
+    "你是一个智能助手。给定一句话，你需要抽取其中的医学实体，并判断其类别，"
+    "输出格式为[[实体，类别]...]"
+    )
+
+
+#分别提取sentence和labels
+def distinguish(data):
+    data=data.map(lambda x:{
+        'prompt':[
+            {'role':'system','content':SYSTEM_prompt},
+            {'role':'user','content':x["sentence"]},
+            {'role':'assistant','content':json.dumps(x['labels'],ensure_ascii=False)}
+        ]
+    },
+    remove_columns=data.column_names  # ← 关键！
+    )
+    return data
+
+def distinguish_eval(data):
+    data=data.map(lambda x:{
+        'text':x['sentence'],
+        'entities':json.dumps(x['labels'],ensure_ascii=False)
+    },
+    remove_columns=data.column_names  # ← 关键！
+    )
+    return data
---
+++ train/sft_train.py
@@ -0,0 +1,69 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from trl import SFTConfig, SFTTrainer
+from srctwo.data.utils import distinguish,train_data,valid_data
+from peft import LoraConfig, TaskType, get_peft_model
+
+# data=distinguish(train_data)[0]
+# print(f'{data}')
+#{'sentence': '现头昏口苦', 'labels': [['口苦', '临床表现']], 'prompt': [{'content': '你是一个智能助手。给定一句话，你需要抽取其中的医学实体，并判断其类别，输出格式为[[实体，类别]...]', 'role': 'system'}, {'content': '现头昏口苦', 'role': 'user'}, {'content': '[["\\u53e3\\u82e6", "\\u4e34\\u5e8a\\u8868\\u73b0"]]', 'role': 'assistant'}]}
+
+data_train=distinguish(train_data)
+eval_data=distinguish(valid_data)
+
+def train(args):
+    training_args = SFTConfig(
+        output_dir=args.checkpoint_dir,#checkpoint-200 / checkpoint-400 / best_model不是完整模型,LoRA 更新后的可训练参数,在训练到 step 200 时保存的 LoRA adapter 权重,bestmodel就是eval_loss最低的checkpoint
+        learning_rate=args.learning_rate,
+        adam_beta1=args.adam_beta1,
+        adam_beta2=args.adam_beta2,
+        weight_decay=args.weight_decay,
+        warmup_ratio=args.warmup_ratio,
+        lr_scheduler_type=args.lr_scheduler_type,
+        logging_steps=args.logging_steps,
+        bf16=args.bf16,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        max_seq_length=args.max_seq_length,
+        num_train_epochs=args.epochs,
+        save_steps=args.save_steps,
+        save_strategy=args.save_strategy,
+        max_grad_norm=args.max_grad_norm,
+        log_on_each_node=False,
+        report_to="none",
+        load_best_model_at_end=True,
+        evaluation_strategy="steps",
+        eval_steps=100,
+    )
+
+    config=LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+        inference_mode=False,
+        lora_alpha=32,
+        r=8,
+        lora_dropout=0.1
+    )
+
+
+    model=AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path="D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B",
+        torch_dtype=torch.bf16,
+        device_map=None,
+        cache_dir=args.cache_dir
+    ).to("cuda")
+
+    tokenizer=AutoTokenizer.from_pretrained(pretrained_model_name_or_path="D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B",cache_dir=args.cache_dir)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    model=get_peft_model(model,config)
+    #model.print_trainable_parameters()----trainable params: 20,185,088 || all params: 7,635,801,600 || trainable%: 0.2643
+
+    trainer = SFTTrainer(
+        model=model,
+        processing_class=tokenizer,
+        args=training_args,
+        train_dataset=data_train,
+        eval_dataset=eval_data,
+    )
+    trainer.train()
